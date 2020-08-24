8dff63c222a4896597f21d03ea9cedc1ae237f05
kimi.194944@gmail.com <kimi.194944@gmail.com>
2020-08-25 08:07:41 +0900

    復習

--- rihabiri.rb
+++ rihabiri.rb
@@ -1,12 +1,23 @@
+games = []
 def game_resister(games)
   game = {}
   puts "名前を入力してください"
-  game[:names] = gets.chomp
+  game[:name] = gets.chomp
   puts "値段を入力してください"
   game[:price] = gets.to_i
   games << game
 end
-games = {}
+def game_reviewer(games)
+  games.each_with_index do |game,index|
+  puts "#{index+1}:#{game[:names]}"
+  imputs = gets.to_i
+  puts gamesresist(games[imputs-1])
+end
+end
+def gamesresist(game)
+  puts "名前#{game[:name]}"
+  puts "値段#{game[:price]}"
+end
 while true do
   puts "番号を入力してください"
   puts "(i)ゲーム入力"
@@ -16,7 +27,7 @@ while true do
   if inputs == 1
     game_resister(games)
   elsif inputs == 2
-    game_reviewer
+    game_reviewer(games)
   else inputs == 3
     exit
 end
